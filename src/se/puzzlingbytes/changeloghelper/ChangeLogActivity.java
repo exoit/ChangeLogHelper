a37eeffa325c14020838046ef298d04b1b43ceb3
Mikael Olsson <mikael.olsson.se@gmail.com>
2013-07-24 23:32:19 +0200

    Change so css can be supply as a resource id

--- src/se/puzzlingbytes/changeloghelper/ChangeLogActivity.java
+++ src/se/puzzlingbytes/changeloghelper/ChangeLogActivity.java
@@ -21,6 +21,8 @@ public class ChangeLogActivity extends Activity {
 
     public static final String EXTRA_CHANGELOG_CSS_STR = "extra_changelog_css_str";
 
+    public static final String EXTRA_CHANGELOG_CSS_RESID = "extra_changelog_css_resid";
+
     private static final String MIME_TYPE_TEXT_HTML = "text/html";
 
     private static final String ENCODING_UTF8 = "utf-8";
@@ -35,20 +37,37 @@ public class ChangeLogActivity extends Activity {
 
     private WebView mWebView;
 
-    public static Intent getChangeLogIntent(Context context, int changeLogXMLResID,
-            boolean showDate, boolean showCurrent) {
-        return getChangeLogIntent(context, changeLogXMLResID, showDate, showCurrent, null);
+    public static Intent getChangeLogIntent(Context context,
+            int changeLogXMLResID, boolean showDate, boolean showCurrent) {
+        return getChangeLogIntent(context, changeLogXMLResID, showDate,
+                showCurrent, null, -1);
     }
 
-    public static Intent getChangeLogIntent(Context context, int changeLogXMLResID,
-            boolean showDate, boolean showCurrent, String cssStyle) {
+    public static Intent getChangeLogIntent(Context context,
+            int changeLogXMLResID, boolean showDate, boolean showCurrent,
+            int cssStyleResID) {
+        return getChangeLogIntent(context, changeLogXMLResID, showDate,
+                showCurrent, null, cssStyleResID);
+    }
+
+    public static Intent getChangeLogIntent(Context context,
+            int changeLogXMLResID, boolean showDate, boolean showCurrent,
+            String cssStyle) {
+        return getChangeLogIntent(context, changeLogXMLResID, showDate,
+                showCurrent, cssStyle, -1);
+    }
+
+    public static Intent getChangeLogIntent(Context context,
+            int changeLogXMLResID, boolean showDate, boolean showCurrent,
+            String cssStyle, int cssStyleResID) {
         Intent changeLogintent = new Intent(context, ChangeLogActivity.class);
         changeLogintent.putExtra(EXTRA_CHANGELOG_XML_RESID, changeLogXMLResID);
         changeLogintent.putExtra(EXTRA_CHANGELOG_SHOW_DATE, showDate);
         changeLogintent.putExtra(EXTRA_CHANGELOG_SHOW_CURRENT, showCurrent);
-        if (!TextUtils.isEmpty(cssStyle))
-        {
+        if (!TextUtils.isEmpty(cssStyle)) {
             changeLogintent.putExtra(EXTRA_CHANGELOG_CSS_STR, cssStyle);
+        } else if (cssStyleResID > 0) {
+            changeLogintent.putExtra(EXTRA_CHANGELOG_CSS_RESID, cssStyle);
         }
         return changeLogintent;
     }
@@ -60,9 +79,15 @@ public class ChangeLogActivity extends Activity {
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
             mChangeLogResID = intent.getIntExtra(EXTRA_CHANGELOG_XML_RESID, -1);
-            mShowDate = intent.getBooleanExtra(EXTRA_CHANGELOG_SHOW_DATE, false);
-            mShowCurrent = intent.getBooleanExtra(EXTRA_CHANGELOG_SHOW_CURRENT, false);
+            mShowDate = intent
+                    .getBooleanExtra(EXTRA_CHANGELOG_SHOW_DATE, false);
+            mShowCurrent = intent.getBooleanExtra(EXTRA_CHANGELOG_SHOW_CURRENT,
+                    false);
             mStyle = intent.getStringExtra(EXTRA_CHANGELOG_CSS_STR);
+            int styleResID = intent.getIntExtra(EXTRA_CHANGELOG_XML_RESID, -1);
+            if (TextUtils.isEmpty(mStyle) && styleResID > 0) {
+                mStyle = getString(styleResID);
+            }
         }
         mWebView = (WebView) findViewById(R.id.changelog_webview);
     }
@@ -74,7 +99,8 @@ public class ChangeLogActivity extends Activity {
         if (mChangeLogResID > -1) {
             ChangeLogParser changeLogParser = new ChangeLogParser(this);
             ChangeLog changeLog = changeLogParser.parse(mChangeLogResID);
-            webViewData = changeLog.generateChangeLogHTML(mShowDate, mShowCurrent, mStyle);
+            webViewData = changeLog.generateChangeLogHTML(mShowDate,
+                    mShowCurrent, mStyle);
         } else {
             webViewData = getString(R.string.changelog_missing);
         }
